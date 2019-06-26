2ef9d81cdff941581b7e6aecd066f43dcb8fa52b
Rodrigo <rodrigoms2004@gmail.com>
2019-06-26 16:37:42 -0300

    iniciando o sequelize

---
+++ modulo02/src/app.js
@@ -0,0 +1,28 @@
+// usar sucrase para suportar import
+// yarn add sucrase nodemon -D
+// rodar usando: yarn sucrase-node src/server.js
+
+// para usar nodemon ver nodemon.json
+
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
+// module.exports = new App().server
