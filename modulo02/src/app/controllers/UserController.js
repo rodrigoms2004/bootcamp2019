a3951056d06353e7969aa350f4b28ca703eaff38
Rodrigo <rodrigoms2004@gmail.com>
2019-06-27 16:29:00 -0300

    faltando update e validação de dados de entrada

---
+++ modulo02/src/app/controllers/UserController.js
@@ -0,0 +1,29 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    // userId é obtido no token durante autenticação e colocado na requisição req
+    console.log(req.userId);
+
+    return res.json({ ok: true });
+  }
+}
+
+export default new UserController();
