14433c6ce22bf7ab5b3dcae3cc1e601891e40d1c
Rodrigo <rodrigoms2004@gmail.com>
2019-07-06 16:08:38 -0300

    fix: mudando pastas

--- modulo02_03/src/app/jobs/CancellationMail.js
+++ modulo02_03/src/app/jobs/CancellationMail.js
@@ -10,7 +10,7 @@ class CancellationMail {
   async handle({ data }) {
     const { appointment } = data;
 
-    console.log('A fila executou');
+    // console.log('A fila executou');
 
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
