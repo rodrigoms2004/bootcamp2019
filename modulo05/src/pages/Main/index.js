e0ad068cfb6477739d097dbb1b7619e0fdb2cf8c
Rodrigo <rodrigoms2004@gmail.com>
2019-07-10 20:46:39 -0300

    done: adicionando repositorios

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,40 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa'
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true })
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    })
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
 
 
-      <Form onSubmit={() => {}}>
-        <input
-          type="text"
-          placeholder="Adicionar repositório"
-        />
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
 
-        <SubmitButton>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
+          <SubmitButton loading={loading}>
+            { loading ? (
+              <FaSpinner color='#FFF' size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
 
-    </Container>
-  );
+      </Container>
+    );
+  }
 }
-
-export default Main;
-
-// function Main() {
-//   return (
-//     <Title error={true}>
-//       Main
-//       <small>menor</small>
-//     </Title>
-//   );
-// }
