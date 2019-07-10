91d84534b597ad5582aa2fd7246cc4ae568183a5
Rodrigo <rodrigoms2004@gmail.com>
2019-07-10 19:53:21 -0300

    main estilizado

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
   return (
-    <Title>
-      Hello World
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+
+      <Form onSubmit={() => {}}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+        />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+
+    </Container>
   );
 }
 
