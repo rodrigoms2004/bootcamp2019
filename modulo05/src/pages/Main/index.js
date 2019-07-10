a8fc78d96305aa2811ccd5f779b96ae93aca1246
Rodrigo <rodrigoms2004@gmail.com>
2019-07-10 19:02:59 -0300

    estilização global

---
+++ modulo05/src/pages/Main/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+function Main() {
+  return (
+    <Title>
+      Hello World
+    </Title>
+  );
+}
+
+export default Main;
+
+// function Main() {
+//   return (
+//     <Title error={true}>
+//       Main
+//       <small>menor</small>
+//     </Title>
+//   );
+// }
