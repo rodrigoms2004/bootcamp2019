73c17ed88e4c5191703576baf169ae2a4ae8ab94
Rodrigo <rodrigoms2004@gmail.com>
2019-07-17 16:21:08 -0300

    modulo06 done ReactNative

--- modulo06/src/pages/Main/index.js
+++ modulo06/src/pages/Main/index.js
@@ -1,12 +1,135 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import { Container } from './styles';
+// lista de icones
+// https://oblador.github.io/react-native-vector-icons/
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
-export default function Main() {
-  return <Container />;
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
+
+export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    newUser: '',
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    // console.tron.log(this.props);
+    const users = await AsyncStorage.getItem('users');
+
+    if (users) {
+      this.setState({ users: JSON.parse(users) });
+    }
+  }
+
+  async componentDidUpdate(_, prevState) {
+    const { users } = this.state;
+
+    if (prevState.users !== users) {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
+
+  handleAddUser = async () => {
+    // console.tron.log(this.state.newUser);
+
+    const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+      loading: false,
+    });
+
+    Keyboard.dismiss(); // clicando no botão o teclado some
+  };
+
+  handleNavigate = user => {
+    // ver  console.tron em componentDidMount, no Rectotron ver propriedade navigation.navigate()
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
+  };
+
+  render() {
+    const { users, newUser, loading } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            // habilita botão de return do teclado do telefone para executar mesma função do botão
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          {/* como não existe Form no react native, o evento vai no botão e não no formulário */}
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <Icon name="add" size={20} color="#FFF" />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
+        />
+      </Container>
+    );
+  }
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
