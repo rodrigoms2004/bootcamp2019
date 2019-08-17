b3cdb694170d1c75f1ab657cc5e2e5cb30ad37d2
Rodrigo <rodrigoms2004@gmail.com>
2019-08-17 19:07:39 -0300

    Hook useState

--- modulo08/src/App.js
+++ modulo08/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  // <> é um fragment para ter mais de um componente em uma função
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
