37f59c06393754dadbd2329f54e890095099fcb8
Rodrigo <rodrigoms2004@gmail.com>
2019-08-17 19:33:44 -0300

    Hook useEffect

--- modulo08/src/App.js
+++ modulo08/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [tech, setTech] = useState(['ReactJS', 'React Native']);
@@ -9,6 +9,27 @@ function App() {
     setNewTech('');
   }
 
+  // busca os dados já armazenados no storage, componentDidMount do Redux
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+
+    // componentWillUnmount do Redux, executar alguma função
+    // após que o componente deixa de ser montado
+    // use effect
+    // return () => {
+    // document.removeEventListener()
+    // };
+  }, []);
+
+  // armazena dados no storage componentDidUpdate do Redux
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
   // <> é um fragment para ter mais de um componente em uma função
   return (
     <>
@@ -17,6 +38,8 @@ function App() {
           <li key={t}>{t}</li>
         ))}
       </ul>
+      <strong>Você tem {tech.length} tecnologias</strong>
+      <br />
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>
         Adicionar
